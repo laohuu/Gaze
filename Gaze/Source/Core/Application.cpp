f702ab9fe6c0fd11469acad9ab27dcebcce2203b
Hanghai Hu <14170811@qq.com>
2023-05-22 15:55:15 +0800

    Added buffer layout descriptors for vertex buffers.

--- Gaze/Source/Core/Application.cpp
+++ Gaze/Source/Core/Application.cpp
@@ -11,6 +11,36 @@ namespace Gaze {
 
     Application *Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+        switch (type) {
+            case Gaze::ShaderDataType::Float:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Float2:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Float3:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Float4:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Mat3:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Mat4:
+                return GL_FLOAT;
+            case Gaze::ShaderDataType::Int:
+                return GL_INT;
+            case Gaze::ShaderDataType::Int2:
+                return GL_INT;
+            case Gaze::ShaderDataType::Int3:
+                return GL_INT;
+            case Gaze::ShaderDataType::Int4:
+                return GL_INT;
+            case Gaze::ShaderDataType::Bool:
+                return GL_BOOL;
+        }
+
+        GZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
     Application::Application() {
         GZ_CORE_ASSERT(!s_Instance, "Application already exists!");
         s_Instance = this;
@@ -28,32 +58,52 @@ namespace Gaze {
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.0f, 0.5f, 0.0f
+        float vertices[3 * 7] = {
+                -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+                0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+                0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
         };
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
+        {
+            BufferLayout layout = {
+                    {ShaderDataType::Float3, "a_Position"},
+                    {ShaderDataType::Float4, "a_Color"}
+            };
 
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+            m_VertexBuffer->SetLayout(layout);
+        }
 
+        uint32_t index = 0;
+        const auto &layout = m_VertexBuffer->GetLayout();
+        for (const auto &element: layout) {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index,
+                                  element.GetComponentCount(),
+                                  ShaderDataTypeToOpenGLBaseType(element.Type),
+                                  element.Normalized ? GL_TRUE : GL_FALSE,
+                                  layout.GetStride(),
+                                  (const void *) element.Offset);
+            index++;
+        }
 
         uint32_t indices[3] = {0, 1, 2};
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-
 
         std::string vertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0f);
 			}
 		)";
@@ -62,14 +112,18 @@ namespace Gaze {
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+				color = v_Color;
 			}
 		)";
 
-        m_Shader = std::make_unique<Shader>(vertexSrc, fragmentSrc);
+        m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
 
     Application::~Application() {
