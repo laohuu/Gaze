2d18d7518d5af0ca34598334fac8fe4faa7b74bc
Hanghai Hu <14170811@qq.com>
2023-05-29 23:08:37 +0800

    Added check for OpenGL version

--- Gaze/Source/Platform/OpenGL/OpenGLContext.cpp
+++ Gaze/Source/Platform/OpenGL/OpenGLContext.cpp
@@ -15,7 +15,16 @@ namespace Gaze {
     void OpenGLContext::Init() {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-        GZ_CORE_ASSERT(status, "Failed to initialize Glad!")
+        GZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+
+        GZ_CORE_INFO("OpenGL Info:");
+        GZ_CORE_INFO("  Vendor: {0}", (char *) glGetString(GL_VENDOR));
+        GZ_CORE_INFO("  Renderer: {0}", (char *) glGetString(GL_RENDERER));
+        GZ_CORE_INFO("  Version: {0}", (char *) glGetString(GL_VERSION));
+
+        GZ_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5),
+                       "Hazel requires at least OpenGL version 4.5!");
     }
 
     void OpenGLContext::SwapBuffers() {
