55272c32489b4cdd70bd489d8f19ca3dbcd23dbf
Hanghai Hu <14170811@qq.com>
2023-05-29 23:02:15 +0800

    Automated platform detection using predefined macro's

--- Gaze/Source/Core/Core.h
+++ Gaze/Source/Core/Core.h
@@ -3,14 +3,54 @@
 
 #include <memory>
 
-#ifdef GZ_DEBUG
-#ifdef GZ_PLATFORM_WINDOWS
-#define GZ_ASSERT(x, ...) { if(!(x)) { GZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define GZ_CORE_ASSERT(x, ...) { if(!(x)) { GZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
+// Platform detection using predefined macros
+#ifdef _WIN32
+    /* Windows x64/x86 */
+    #ifdef _WIN64
+        /* Windows x64  */
+        #define GZ_PLATFORM_WINDOWS
+    #else
+        /* Windows x86 */
+        #error "x86 Builds are not supported!"
+    #endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define GZ_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define GZ_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+/* We also have to check __ANDROID__ before __linux__
+ * since android is based on the linux kernel
+ * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#define GZ_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define GZ_PLATFORM_LINUX
+	#error "Linux is not supported!"
 #else
-#define GZ_ASSERT(x, ...) { if(!(x)) { GZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define GZ_CORE_ASSERT(x, ...) { if(!(x)) { GZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
-#endif
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif // _WIN32
+
+#ifdef GZ_DEBUG
+    #ifdef GZ_PLATFORM_WINDOWS
+        #define GZ_ASSERT(x, ...) { if(!(x)) { GZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+        #define GZ_CORE_ASSERT(x, ...) { if(!(x)) { GZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+    #else
+        #define GZ_ASSERT(x, ...) { if(!(x)) { GZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
+        #define GZ_CORE_ASSERT(x, ...) { if(!(x)) { GZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
+    #endif
 #else
 #define GZ_ASSERT(x, ...)
 #define GZ_CORE_ASSERT(x, ...)
