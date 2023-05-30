4cf7cc9eec69835dd0a5999883a51cb272a90116
Hanghai Hu <14170811@qq.com>
2023-05-30 22:28:54 +0800

    Log messages are now additionally sent to Hazel.log file

--- Gaze/Source/Core/Log.cpp
+++ Gaze/Source/Core/Log.cpp
@@ -1,6 +1,8 @@
 #include "GazePCH.h"
 #include "Log.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Gaze {
 
@@ -8,12 +10,23 @@ namespace Gaze {
     Gaze::Ref<spdlog::logger> Log::s_ClientLogger;
 
     void Log::Init() {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("GazeEngine");
-        s_CoreLogger->set_level(spdlog::level::trace);
 
-        s_ClientLogger = spdlog::stdout_color_mt("Application");
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Gaze.log", true));
+
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+        s_CoreLogger = std::make_shared<spdlog::logger>("GAZE", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
+        s_CoreLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
+
+        s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_ClientLogger);
         s_ClientLogger->set_level(spdlog::level::trace);
+        s_ClientLogger->flush_on(spdlog::level::trace);
     }
 
 }
--- Gaze/Source/Core/Log.h
+++ Gaze/Source/Core/Log.h
@@ -2,6 +2,11 @@
 #define GAZEENGINE_LOG_H
 
 #include "Core.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include "glm/gtx/string_cast.hpp"
+
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
 
@@ -18,6 +23,21 @@ namespace Gaze {
         static Gaze::Ref<spdlog::logger> s_CoreLogger;
         static Gaze::Ref<spdlog::logger> s_ClientLogger;
     };
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, const glm::vec<L, T, Q> &vector) {
+    return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, const glm::mat<C, R, T, Q> &matrix) {
+    return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, glm::qua<T, Q> quaternion) {
+    return os << glm::to_string(quaternion);
 }
 
 // Core log macros
