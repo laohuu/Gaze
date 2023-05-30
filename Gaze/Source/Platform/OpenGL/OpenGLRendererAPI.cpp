5aedc33335f149cbcedbc5f4387f3d8ea58ba4d3
Hanghai Hu <14170811@qq.com>
2023-05-30 23:29:27 +0800

    Logging OpenGL messages

--- Gaze/Source/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Gaze/Source/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -5,11 +5,47 @@
 
 namespace Gaze {
 
+    void OpenGLMessageCallback(
+            unsigned source,
+            unsigned type,
+            unsigned id,
+            unsigned severity,
+            int length,
+            const char *message,
+            const void *userParam) {
+        switch (severity) {
+            case GL_DEBUG_SEVERITY_HIGH:
+                GZ_CORE_CRITICAL(message);
+                return;
+            case GL_DEBUG_SEVERITY_MEDIUM:
+                GZ_CORE_ERROR(message);
+                return;
+            case GL_DEBUG_SEVERITY_LOW:
+                GZ_CORE_WARN(message);
+                return;
+            case GL_DEBUG_SEVERITY_NOTIFICATION:
+                GZ_CORE_TRACE(message);
+                return;
+        }
+
+        GZ_CORE_ASSERT(false, "Unknown severity level!");
+    }
+
     void OpenGLRendererAPI::Init() {
         GZ_PROFILE_FUNCTION();
 
+#ifdef GZ_DEBUG
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
+#endif
+
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_LINE_SMOOTH);
     }
 
     void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
