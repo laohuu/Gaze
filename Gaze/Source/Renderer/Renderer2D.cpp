b7f6f454c40347f452788516758c9ccb1a425d0e
Hanghai Hu <14170811@qq.com>
2023-06-01 00:31:45 +0800

    Added textures to batch renderer

--- Gaze/Source/Renderer/Renderer2D.cpp
+++ Gaze/Source/Renderer/Renderer2D.cpp
@@ -11,13 +11,15 @@ namespace Gaze {
         glm::vec3 Position;
         glm::vec4 Color;
         glm::vec2 TexCoord;
-        // TODO: texid
+        float TexIndex;
+        float TilingFactor;
     };
 
     struct Renderer2DData {
         const uint32_t MaxQuads = 10000;
         const uint32_t MaxVertices = MaxQuads * 4;
         const uint32_t MaxIndices = MaxQuads * 6;
+        static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
         Gaze::Ref<VertexArray> QuadVertexArray;
         Gaze::Ref<VertexBuffer> QuadVertexBuffer;
@@ -27,6 +29,9 @@ namespace Gaze {
         uint32_t QuadIndexCount = 0;
         QuadVertex *QuadVertexBufferBase = nullptr;
         QuadVertex *QuadVertexBufferPtr = nullptr;
+
+        std::array<Gaze::Ref<Texture2D>, MaxTextureSlots> TextureSlots;
+        uint32_t TextureSlotIndex = 1; // 0 = white texture
     };
 
     static Renderer2DData s_Data;
@@ -40,8 +45,11 @@ namespace Gaze {
         s_Data.QuadVertexBuffer->SetLayout({
                                                    {ShaderDataType::Float3, "a_Position"},
                                                    {ShaderDataType::Float4, "a_Color"},
-                                                   {ShaderDataType::Float2, "a_TexCoord"}
+                                                   {ShaderDataType::Float2, "a_TexCoord"},
+                                                   {ShaderDataType::Float,  "a_TexIndex"},
+                                                   {ShaderDataType::Float,  "a_TilingFactor"}
                                            });
+
         s_Data.QuadVertexArray->AddVertexBuffer(s_Data.QuadVertexBuffer);
 
         s_Data.QuadVertexBufferBase = new QuadVertex[s_Data.MaxVertices];
@@ -68,14 +76,26 @@ namespace Gaze {
         uint32_t whiteTextureData = 0xffffffff;
         s_Data.WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
 
+        int32_t samplers[s_Data.MaxTextureSlots];
+        for (uint32_t i = 0; i < s_Data.MaxTextureSlots; i++)
+            samplers[i] = i;
+
         s_Data.TextureShader = Shader::Create(
                 "C:/Users/hangh/Documents/GitHub/Gaze/Sandbox/Assets/Shaders/Texture.glsl");
         s_Data.TextureShader->Bind();
-        s_Data.TextureShader->SetInt("u_Texture", 0);
+        s_Data.TextureShader->SetIntArray("u_Textures", samplers, s_Data.MaxTextureSlots);
+        // Set all texture slots to 0
+        s_Data.TextureSlots[0] = s_Data.WhiteTexture;
+        s_Data.TextureSlotIndex = 1;
     }
 
     void Renderer2D::Shutdown() {
         GZ_PROFILE_FUNCTION();
+
+        s_Data.QuadIndexCount = 0;
+        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+        s_Data.TextureSlotIndex = 1;
     }
 
     void Renderer2D::BeginScene(const OrthographicCamera &camera) {
@@ -98,6 +118,10 @@ namespace Gaze {
     }
 
     void Renderer2D::Flush() {
+        // Bind textures
+        for (uint32_t i = 0; i < s_Data.TextureSlotIndex; i++)
+            s_Data.TextureSlots[i]->Bind(i);
+
         RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
     }
 
@@ -108,24 +132,35 @@ namespace Gaze {
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const glm::vec4 &color) {
         GZ_PROFILE_FUNCTION();
 
+        const float texIndex = 0.0f; // White Texture
+        const float tilingFactor = 1.0f;
+
         s_Data.QuadVertexBufferPtr->Position = position;
         s_Data.QuadVertexBufferPtr->Color = color;
         s_Data.QuadVertexBufferPtr->TexCoord = {0.0f, 0.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = texIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadVertexBufferPtr->Position = {position.x + size.x, position.y, 0.0f};
         s_Data.QuadVertexBufferPtr->Color = color;
         s_Data.QuadVertexBufferPtr->TexCoord = {1.0f, 0.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = texIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadVertexBufferPtr->Position = {position.x + size.x, position.y + size.y, 0.0f};
         s_Data.QuadVertexBufferPtr->Color = color;
         s_Data.QuadVertexBufferPtr->TexCoord = {1.0f, 1.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = texIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadVertexBufferPtr->Position = {position.x, position.y + size.y, 0.0f};
         s_Data.QuadVertexBufferPtr->Color = color;
         s_Data.QuadVertexBufferPtr->TexCoord = {0.0f, 1.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = texIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
@@ -151,19 +186,65 @@ namespace Gaze {
                               float tilingFactor, const glm::vec4 &tintColor) {
         GZ_PROFILE_FUNCTION();
 
-        s_Data.TextureShader->Bind();
+        constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
-        s_Data.TextureShader->SetFloat4("u_Color", tintColor);
-        s_Data.TextureShader->SetFloat("u_TilingFactor", tilingFactor);
-        texture->Bind();
-        glm::mat4 transform =
-                glm::translate(glm::mat4(1.0f), position)
-                * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1.0f});
-        s_Data.TextureShader->SetMat4("u_Transform", transform);
+        float textureIndex = 0.0f;
+        for (uint32_t i = 1; i < s_Data.TextureSlotIndex; i++) {
+            if (*s_Data.TextureSlots[i].get() == *texture.get()) {
+                textureIndex = (float) i;
+                break;
+            }
+        }
 
-        texture->Bind();
-        s_Data.QuadVertexArray->Bind();
-        RenderCommand::DrawIndexed(s_Data.QuadVertexArray);
+        if (textureIndex == 0.0f) {
+            textureIndex = (float) s_Data.TextureSlotIndex;
+            s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
+            s_Data.TextureSlotIndex++;
+        }
+
+        s_Data.QuadVertexBufferPtr->Position = position;
+        s_Data.QuadVertexBufferPtr->Color = color;
+        s_Data.QuadVertexBufferPtr->TexCoord = {0.0f, 0.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
+        s_Data.QuadVertexBufferPtr++;
+
+        s_Data.QuadVertexBufferPtr->Position = {position.x + size.x, position.y, 0.0f};
+        s_Data.QuadVertexBufferPtr->Color = color;
+        s_Data.QuadVertexBufferPtr->TexCoord = {1.0f, 0.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
+        s_Data.QuadVertexBufferPtr++;
+
+        s_Data.QuadVertexBufferPtr->Position = {position.x + size.x, position.y + size.y, 0.0f};
+        s_Data.QuadVertexBufferPtr->Color = color;
+        s_Data.QuadVertexBufferPtr->TexCoord = {1.0f, 1.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
+        s_Data.QuadVertexBufferPtr++;
+
+        s_Data.QuadVertexBufferPtr->Position = {position.x, position.y + size.y, 0.0f};
+        s_Data.QuadVertexBufferPtr->Color = color;
+        s_Data.QuadVertexBufferPtr->TexCoord = {0.0f, 1.0f};
+        s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+        s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
+        s_Data.QuadVertexBufferPtr++;
+
+        s_Data.QuadIndexCount += 6;
+
+//        s_Data.TextureShader->Bind();
+//
+//        s_Data.TextureShader->SetFloat4("u_Color", tintColor);
+//        s_Data.TextureShader->SetFloat("u_TilingFactor", tilingFactor);
+//        texture->Bind();
+//        glm::mat4 transform =
+//                glm::translate(glm::mat4(1.0f), position)
+//                * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1.0f});
+//        s_Data.TextureShader->SetMat4("u_Transform", transform);
+//
+//        texture->Bind();
+//        s_Data.QuadVertexArray->Bind();
+//        RenderCommand::DrawIndexed(s_Data.QuadVertexArray);
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation,
