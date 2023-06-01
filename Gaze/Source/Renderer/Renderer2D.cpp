f1b3d80a829a1160475fc228b446571e638ddbe2
Hanghai Hu <14170811@qq.com>
2023-06-01 14:45:17 +0800

    Renderer2D: added draw call splitting and stats

--- Gaze/Source/Renderer/Renderer2D.cpp
+++ Gaze/Source/Renderer/Renderer2D.cpp
@@ -16,9 +16,9 @@ namespace Gaze {
     };
 
     struct Renderer2DData {
-        const uint32_t MaxQuads = 10000;
-        const uint32_t MaxVertices = MaxQuads * 4;
-        const uint32_t MaxIndices = MaxQuads * 6;
+        static const uint32_t MaxQuads = 20000;
+        static const uint32_t MaxVertices = MaxQuads * 4;
+        static const uint32_t MaxIndices = MaxQuads * 6;
         static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
         Gaze::Ref<VertexArray> QuadVertexArray;
@@ -34,6 +34,8 @@ namespace Gaze {
         uint32_t TextureSlotIndex = 1; // 0 = white texture
 
         glm::vec4 QuadVertexPositions[4];
+
+        Renderer2D::Statistics Stats;
     };
 
     static Renderer2DData s_Data;
@@ -130,6 +132,16 @@ namespace Gaze {
             s_Data.TextureSlots[i]->Bind(i);
 
         RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+        s_Data.Stats.DrawCalls++;
+    }
+
+    void Renderer2D::FlushAndReset() {
+        EndScene();
+
+        s_Data.QuadIndexCount = 0;
+        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+        s_Data.TextureSlotIndex = 1;
     }
 
     void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const glm::vec4 &color) {
@@ -138,6 +150,9 @@ namespace Gaze {
 
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const glm::vec4 &color) {
         GZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+            FlushAndReset();
 
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
@@ -174,6 +189,7 @@ namespace Gaze {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        s_Data.Stats.QuadCount++;
     }
 
     void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const Gaze::Ref<Texture2D> &texture,
@@ -184,6 +200,9 @@ namespace Gaze {
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const Gaze::Ref<Texture2D> &texture,
                               float tilingFactor, const glm::vec4 &tintColor) {
         GZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+            FlushAndReset();
 
         constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
@@ -233,6 +252,7 @@ namespace Gaze {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        s_Data.Stats.QuadCount++;
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation,
@@ -243,6 +263,9 @@ namespace Gaze {
     void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation,
                                      const glm::vec4 &color) {
         GZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+            FlushAndReset();
 
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
@@ -280,6 +303,7 @@ namespace Gaze {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        s_Data.Stats.QuadCount++;
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation,
@@ -290,6 +314,9 @@ namespace Gaze {
     void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation,
                                      const Ref <Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor) {
         GZ_PROFILE_FUNCTION();
+
+        if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+            FlushAndReset();
 
         constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
@@ -340,5 +367,14 @@ namespace Gaze {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        s_Data.Stats.QuadCount++;
+    }
+
+    void Renderer2D::ResetStats() {
+        memset(&s_Data.Stats, 0, sizeof(Statistics));
+    }
+
+    Renderer2D::Statistics Renderer2D::GetStats() {
+        return s_Data.Stats;
     }
 } // Gaze
--- Sandbox/Source/Sandbox2D.cpp
+++ Sandbox/Source/Sandbox2D.cpp
@@ -32,6 +32,8 @@ void Sandbox2D::OnUpdate(Gaze::Timestep ts) {
 
 
     // Render
+    Gaze::Renderer2D::ResetStats();
+
     {
         GZ_PROFILE_SCOPE("Renderer Prep");
         Gaze::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
@@ -49,9 +51,18 @@ void Sandbox2D::OnUpdate(Gaze::Timestep ts) {
         Gaze::Renderer2D::DrawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
         Gaze::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         Gaze::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-        Gaze::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
+        Gaze::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {20.0f, 20.0f}, m_CheckerboardTexture, 10.0f);
         Gaze::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
 
+        Gaze::Renderer2D::EndScene();
+
+        Gaze::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        for (float y = -5.0f; y < 5.0f; y += 0.5f) {
+            for (float x = -5.0f; x < 5.0f; x += 0.5f) {
+                glm::vec4 color = {(x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f};
+                Gaze::Renderer2D::DrawQuad({x, y}, {0.45f, 0.45f}, color);
+            }
+        }
         Gaze::Renderer2D::EndScene();
     }
 
@@ -61,6 +72,14 @@ void Sandbox2D::OnImGuiRender() {
     GZ_PROFILE_FUNCTION();
 
     ImGui::Begin("Settings");
+
+    auto stats = Gaze::Renderer2D::GetStats();
+    ImGui::Text("Renderer2D Stats:");
+    ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+    ImGui::Text("Quads: %d", stats.QuadCount);
+    ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+    ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
     ImGui::End();
 }
