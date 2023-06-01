3e9addd87f70613fbe73cc8ee6aae671ad340605
Hanghai Hu <14170811@qq.com>
2023-06-01 14:20:47 +0800

    Renderer2D can now render rotated quads in batches

--- Sandbox/Source/Sandbox2D.cpp
+++ Sandbox/Source/Sandbox2D.cpp
@@ -40,13 +40,17 @@ void Sandbox2D::OnUpdate(Gaze::Timestep ts) {
 
     {
         GZ_PROFILE_SCOPE("Renderer Draw");
+
+        static float rotation = 0.0f;
+        rotation += ts * 50.0f;
+
         Gaze::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-//        Gaze::Renderer2D::DrawRotatedQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, glm::radians(45.f), {0.8f, 0.2f, 0.3f, 1.0f});
+        Gaze::Renderer2D::DrawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
         Gaze::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         Gaze::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-        Gaze::Renderer2D::DrawQuad({-5.0f, -5.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
-        Gaze::Renderer2D::DrawQuad({-0.5f, -0.5f, 0.0f}, {1.0f, 1.0f}, m_CheckerboardTexture, 20.0f);
+        Gaze::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
+        Gaze::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
 
         Gaze::Renderer2D::EndScene();
     }
